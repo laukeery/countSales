2a3763a5b8a4702f971dcc781d30ddecdb6b74a2
laukeery <nassklif@yandex.ru>
2022-09-06 23:45:10 +0300

    переменная summa

--- countSales.py
+++ countSales.py
@@ -1,19 +1,19 @@
 file = open('inf_22_10_20_26.txt')
 a = [] 
 m = int(file.readline())  # Удаляем первую строку(общее количество купленных товаров)
-summ = 0
+summa = 0
 maxi = 0
 for i in file:
     x = int(i)
     if x <= 50:
-        summ += x
+        summa += x
     else:
         a.append(x)
 a.sort()
 for i in range(len(a)):
     if i < len(a)//2:
-        summ += a[i] * 0.75
+        summa += a[i] * 0.75
         maxi = round(a[i])
     else:
         s += a[i]
-print(round(summ), maxi)
+print(round(summa), maxi)
